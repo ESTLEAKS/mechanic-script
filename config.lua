d2f926c6751534f93fbdaf9a0d756cf95eff3859
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-08-15 21:58:01 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,18 +5,13 @@ Config.Shops = {
         coords = vector3(117.0, -1300.0, 29.0),
         label = 'Pillbox Mechanic',
         uiLocation = vector3(117.0, -1300.0, 29.0),
-    },
-    ['sandy'] = {
-        coords = vector3(1737.0, 3709.0, 34.0),
-        label = 'Sandy Mechanic',
-        uiLocation = vector3(1737.0, 3709.0, 34.0),
     }
 }
 
 Config.JobGrades = {
     ['boss'] = { label = 'Boss', canCustomize = true, canManage = true },
     ['supervisor'] = { label = 'Supervisor', canCustomize = true, canManage = false },
-    ['mechanic'] = { label = 'Mechanic', canCustomize = false, canManage = false }
+    ['mechanic'] = { label = 'Mechanic', canCustomize = true, canManage = false }
 }
 
 Config.Salary = {
@@ -24,6 +19,10 @@ Config.Salary = {
     supervisor = 800,
     boss = 1200
 }
+
+Config.RequiredHours = 6 * 60 * 60 -- 6 hours in seconds
+Config.SafeAmount = 100000 -- Starting safe balance
+
 
 
 
