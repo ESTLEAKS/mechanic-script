982e1df03e34486e26da9a72c488263bb4fa37fb
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-08-15 20:11:35 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Southside'
+description 'Advanced Mechanic Job with ox_lib UI'
+
+shared_script '@ox_lib/init.lua'
+shared_script 'config.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
