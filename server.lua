e2fa2cb1cbe9b7a6015deb615b47eb92a8c4e12c
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-08-15 21:58:41 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,43 +1,98 @@
 local employeeData = {}
+local jsonFile = 'data.json'
+local safeBalance = Config.SafeAmount
+
+local function loadData()
+    local file = LoadResourceFile(GetCurrentResourceName(), jsonFile)
+    if file then employeeData = json.decode(file) or {} end
+end
+
+local function saveData()
+    SaveResourceFile(GetCurrentResourceName(), jsonFile, json.encode(employeeData), -1)
+end
+
+loadData()
 
 RegisterNetEvent('mechanic:getGrade', function()
     local src = source
-    local identifier = GetPlayerIdentifier(src)
-    local grade = employeeData[identifier] and employeeData[identifier].grade or 'mechanic'
+    local id = GetPlayerIdentifier(src)
+    local grade = employeeData[id] and employeeData[id].grade or 'mechanic'
     TriggerClientEvent('mechanic:openMenu', src, grade)
 end)
 
+RegisterNetEvent('mechanic:clockIn', function()
+    local src = source
+    local id = GetPlayerIdentifier(src)
+    employeeData[id] = employeeData[id] or { grade = 'mechanic', clockedIn = false, time = 0 }
+    employeeData[id].clockedIn = true
+    employeeData[id].startTime = os.time()
+    saveData()
+    TriggerClientEvent('mechanic:notify', src, 'Clocked in!', 'success')
+end)
+
+RegisterNetEvent('mechanic:clockOut', function()
+    local src = source
+    local id = GetPlayerIdentifier(src)
+    if employeeData[id] and employeeData[id].clockedIn then
+        local session = os.time() - (employeeData[id].startTime or os.time())
+        employeeData[id].time = (employeeData[id].time or 0) + session
+        employeeData[id].clockedIn = false
+        saveData()
+        TriggerClientEvent('mechanic:notify', src, 'Clocked out!', 'info')
+    end
+end)
+
+RegisterNetEvent('mechanic:claimSalary', function()
+    local src = source
+    local id = GetPlayerIdentifier(src)
+    local data = employeeData[id]
+    if data and data.time >= Config.RequiredHours then
+        local salary = Config.Salary[data.grade]
+        TriggerClientEvent('mechanic:notify', src, 'Salary claimed: $' .. salary, 'success')
+        data.time = 0
+        saveData()
+    else
+        TriggerClientEvent('mechanic:notify', src, 'You need 6 hours of work to claim salary.', 'error')
+    end
+end)
+
+RegisterNetEvent('mechanic:customizeVehicle', function()
+    local src = source
+    TriggerClientEvent('mechanic:notify', src, 'Vehicle upgraded: Turbo, Engine, Nitrous installed!', 'success')
+end)
+
 RegisterNetEvent('mechanic:hire', function(targetId)
-    local identifier = GetPlayerIdentifier(targetId)
-    employeeData[identifier] = { grade = 'mechanic', clockedIn = true }
+    local id = GetPlayerIdentifier(targetId)
+    employeeData[id] = { grade = 'mechanic', clockedIn = false, time = 0 }
+    saveData()
 end)
 
 RegisterNetEvent('mechanic:fire', function(targetId)
-    local identifier = GetPlayerIdentifier(targetId)
-    employeeData[identifier] = nil
+    local id = GetPlayerIdentifier(targetId)
+    employeeData[id] = nil
+    saveData()
 end)
 
 RegisterNetEvent('mechanic:manageRoles', function(targetId, newGrade)
-    local identifier = GetPlayerIdentifier(targetId)
-    if employeeData[identifier] then
-        employeeData[identifier].grade = newGrade
+    local id = GetPlayerIdentifier(targetId)
+    if employeeData[id] then
+        employeeData[id].grade = newGrade
+        saveData()
     end
 end)
 
-RegisterNetEvent('mechanic:clocked', function()
-    local list = {}
-    for id, data in pairs(employeeData) do
-        if data.clockedIn then
-            table.insert(list, { id = id, grade = Config.JobGrades[data.grade].label })
-        end
-    end
-    TriggerClientEvent('ox_lib:notify', source, { type = 'info', description = json.encode(list) })
-end)
-
-RegisterNetEvent('mechanic:salary', function()
+RegisterNetEvent('mechanic:checkSafe', function()
     local src = source
-    local identifier = GetPlayerIdentifier(src)
-    local grade = employeeData[identifier] and employeeData[identifier].grade or 'mechanic'
-    local salary = Config.Salary[grade]
-    TriggerClientEvent('ox_lib:notify', src, { type = 'info', description = 'Your salary: $' .. salary })
+    TriggerClientEvent('mechanic:notify', src, 'Safe Balance: $' .. safeBalance, 'info')
+end)
+
+RegisterNetEvent('mechanic:payEmployee', function(targetId, amount)
+    local src = source
+    if safeBalance >= amount then
+        safeBalance = safeBalance - amount
+        TriggerClientEvent('mechanic:notify', src, 'Paid employee $' .. amount, 'success')
+        saveData()
+    else
+        TriggerClientEvent('mechanic:notify', src, 'Not enough in safe.', 'error')
+    end
 end)
