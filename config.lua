6b89d2fe21c1b648b1eb8cec3a8947c692280c13
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-08-15 20:11:57 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,18 @@
+Config.Shops = {
+    ['pillbox'] = {
+        coords = vector3(117.0, -1300.0, 29.0),
+        label = 'Pillbox Mechanic',
+        uiLocation = vector3(117.0, -1300.0, 29.0),
+    },
+    ['sandy'] = {
+        coords = vector3(1737.0, 3709.0, 34.0),
+        label = 'Sandy Mechanic',
+        uiLocation = vector3(1737.0, 3709.0, 34.0),
+    }
+}
+
+Config.Salary = {
+    mechanic = 500,
+    supervisor = 800,
+    manager = 1000
+}
