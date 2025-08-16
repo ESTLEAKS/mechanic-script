0d6b0c7b49e589d75d83411a123dca610d2a1a2a
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-08-15 21:56:47 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Southside'
-description 'Advanced Mechanic Job with ox_lib UI'
+description 'Mechanic Job with ox_lib and okokNotify'
 
 shared_script '@ox_lib/init.lua'
 shared_script 'config.lua'
